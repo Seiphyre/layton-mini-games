7b4028df1da1b97aa8e93b968f3aaec17a575c34
Victor <victor.danneels@epitech.eu>
2025-12-04 17:23:46 +0900

    Last remodeling changes.

--- Assets/Data/ShopItemListData/ShopItemListData.cs
+++
@@ -1,6 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "ShopItemListData")]
-public class ShopItemListData : ListData<PieceData> {  }
